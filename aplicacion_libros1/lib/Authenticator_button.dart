f5d1b1dbdce5e55569d84bbccb9ee92a9f0178f7
PabloLZ <pablo.lozanomon@gmail.com>
2022-05-25 22:20:22 +0200

    Login-v1
    Login add to the main app

---
+++ aplicacion_libros1/lib/Authenticator_button.dart
@@ -0,0 +1,30 @@
+import 'package:aplicacion_libros1/Authenticator.dart';
+import 'package:aplicacion_libros1/HomePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+
+
+class GoogleAuthenticator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Firebase.initializeApp();
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () async {
+            User? user = await Authenticator.iniciarSesion(
+              context: context);
+            print(user?.displayName);
+            Get.toNamed('/second');
+          },
+          color: Colors.blue,
+          child: const Icon(FontAwesomeIcons.google),
+          textColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
